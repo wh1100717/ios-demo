1f3c871a6682e11b51dd96b2af4e1cb8fc786db1
Wang Hao <eric.3126@hotmail.com>
2014-06-23 23:41:27 +0800

    update

---
+++ ios-demo3/ios-demo3-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  ios-demo-Bridging-Header.h
+//  ios-demo3
+//
+//  Created by 王浩 on 14-6-23.
+//  Copyright (c) 2014年 王浩. All rights reserved.
+//
+
---
+++ ios-demo3/ios-demo3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-demo3
+//
+//  Created by 王浩 on 14-6-23.
+//  Copyright (c) 2014年 王浩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
